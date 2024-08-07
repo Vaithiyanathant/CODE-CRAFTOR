5dd128321e39deade59c6f96e945364415b5a96d
Vaithiyanathan <vaithiyanathan30@gmail.com>
2024-08-07 10:31:07 +0530

    security bug

--- CODE-CRAFTOR/src/Components/Profile.jsx
+++ CODE-CRAFTOR/src/Components/Profile.jsx
@@ -2,8 +2,27 @@
 
 import React from "react";
 
-const Profile = () => {
-	return <div>profile</div>;
-};
+export default function Profile() {
+	// Assuming you store user info in localStorage
+	const user = {
+		name: localStorage.getItem("userName"),
+		email: localStorage.getItem("userEmail"),
+		profileImage: localStorage.getItem("profileImage"),
+	};
 
-export default Profile;
+	return (
+		<div className='max-w-2xl mx-auto mt-10 p-4 bg-white rounded-lg shadow-lg'>
+			<div className='flex items-center'>
+				<img
+					className='h-24 w-24 rounded-full mr-4'
+					src={user.profileImage}
+					alt='Profile'
+				/>
+				<div>
+					<h2 className='text-2xl font-bold'>{user.name}</h2>
+					<p className='text-gray-600'>{user.email}</p>
+				</div>
+			</div>
+		</div>
+	);
+}
